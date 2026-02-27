f2b77670eed0a8abd7b8b6b87b4299b614e98530
aziani-alt <aziani@iespompeufabra.cat>
2026-02-27 09:48:37 +0100

    Create sketch.js

---
+++ carabasica/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  ellipse(210,200,230)
+  fill(61, 217, 39)
+  ellipse(170,170,50)
+ fill(38, 14, 125)
+  fill(38,14,125)
+  ellipse(250,170,50,)
+  
+}
